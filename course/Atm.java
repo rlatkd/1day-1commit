59318ec1269b14777f9e5071f8bdca640f166e7e
Sanghun Kim <rlatkdgns042@naver.com>
2024-03-14 22:15:27 +0900

    Update Atm.java

--- course/Atm.java
+++ course/Atm.java
@@ -1,18 +1,13 @@
 import java.util.Scanner;
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class Atm {
-
    public static void main(String[] args) {
           //메뉴를 보여주고 사용자에 선택 ....
          //원하시는 메뉴를 선택하지 않으면 강제로 다시 메뉴를 보여 주세요
-         
          //1.예금
          //2.출금
          //3.잔고
          //4.종료
-         
-         
          //잔액 변수를 생성 (balance)
          //예금 처리  + 누적 (예금 금액을 받아서) balance 누적
          //출금 처리  - 누적 (출금 금액을 받어서) balance에서 뺌
@@ -20,9 +15,7 @@ public class Atm {
          //종료      프로그램 끝 (함수의 종료  , 프로그램의 강제 종료)
       
       Scanner sc = new Scanner(System.in);
-      
       int balance = 0;
-      
       int choice = 0;
       
       do {
@@ -32,7 +25,6 @@ public class Atm {
          System.out.println("3. 잔고");
          System.out.println("4. 종료");
          System.out.print("응답: ");
-         
          choice = Integer.parseInt(sc.nextLine()); 
          
          switch(choice) {
