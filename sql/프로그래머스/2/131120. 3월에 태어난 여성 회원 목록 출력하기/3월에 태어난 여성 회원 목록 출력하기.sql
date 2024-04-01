cdaae50ca9ce5c374e7466ce9e8902461740bfca
Sanghun Kim <rlatkdgns042@naver.com>
2024-04-01 19:44:37 +0900

    renamed:    "\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/2/42577.\342\200\205\354\240\204\355\231\224\353\262\210\355\230\270\342\200\205\353\252\251\353\241\235/README.md" -> "algorithm/\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/2/42577.\342\200\205\354\240\204\355\231\224\353\262\210\355\230\270\342\200\205\353\252\251\353\241\235/README.md" 	renamed:    "\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/2/42577.\342\200\205\354\240\204\355\231\224\353\262\210\355\230\270\342\200\205\353\252\251\353\241\235/\354\240\204\355\231\224\353\262\210\355\230\270\342\200\205\353\252\251\353\241\235.java" -> "algorithm/\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/2/42577.\342\200\205\354\240\204\355\231\224\353\262\210\355\230\270\342\200\205\353\252\251\353\241\235/\354\240\204\355\231\224\353\262\210\355\230\270\342\200\205\353\252\251\353\241\235.java" 	modified:   "sql/\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/2/131120.\342\200\2053\354\233\224\354\227\220\342\200\205\355\203\234\354\226\264\353\202\234\342\200\205\354\227\254\354\204\261\342\200\205\355\232\214\354\233\220\342\200\205\353\252\251\353\241\235\342\200\205\354\266\234\353\240\245\355\225\230\352\270\260/3\354\233\224\354\227\220\342\200\205\355\203\234\354\226\264\353\202\234\342\200\205\354\227\254\354\204\261\342\200\205\355\232\214\354\233\220\342\200\205\353\252\251\353\241\235\342\200\205\354\266\234\353\240\245\355\225\230\352\270\260.sql"

--- sql/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ sql/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,4 +1,4 @@
-SELECT member_id, member_name, gender, to_char(date_of_birth, 'YYYY-MM-DD') as "date_of_birth"
+SELECT member_id,member_name,gender,to_char(date_of_birth,'YYYY-MM-DD') as "date_of_birth"
 from member_profile
-where to_char(date_of_birth, 'MM') = '03' and tlno is not null and gender = 'W'
+where to_char(date_of_birth,'MM') = '03' and tlno is not null and gender = 'W'
 order by member_id asc;
