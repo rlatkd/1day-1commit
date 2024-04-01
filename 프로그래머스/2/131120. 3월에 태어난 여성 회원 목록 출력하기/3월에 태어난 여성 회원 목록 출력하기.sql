855cbd0a95441d3cce46764e3901c0bfaa0ac2a1
Sanghun Kim <rlatkdgns042@naver.com>
2024-04-01 18:49:07 +0900

    Update 3월에 태어난 여성 회원 목록 출력하기.sql

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,4 +1,3 @@
--- 코드를 입력하세요
 SELECT member_id, member_name, gender, to_char(date_of_birth, 'YYYY-MM-DD') as "date_of_birth"
 from member_profile
 where to_char(date_of_birth, 'MM') = '03' and tlno is not null and gender = 'W'
