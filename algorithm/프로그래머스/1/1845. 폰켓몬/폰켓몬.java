5391f5c9d3b50581bea2fa34c3efd55b2aa26756
Sanghun Kim <rlatkdgns042@naver.com>
2024-04-02 18:35:15 +0900

    modified:   "algorithm/\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/1/1845.\342\200\205\355\217\260\354\274\223\353\252\254/\355\217\260\354\274\223\353\252\254.java"

--- algorithm/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ algorithm/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -6,7 +6,7 @@ class Solution {
         //뽑을 수 있는 최대 폰켓몬 수
         int max = nums.length / 2;
         
-        //중복 제거를 위해 HashSet이용
+        //중복 제거를 위해 HashSet 이용
         Set<Integer> hashSet = new HashSet<>();
         
         //중복 제거 하면서 HashSet에 추가
@@ -14,10 +14,10 @@ class Solution {
             hashSet.add(i);
         }
         
-        //최대 폰켓몬이 HashSet 크기 이상이면
-        if (max >= hashSet.size())
-            return hashSet.size(); //HashSet 크기 반환
+        //HashSet 크기가 max보다 크면 max
+        if (hashSet.size() > max)
+            return max;
         else
-            return max; //최대 폰켓몬 수 반환
+            return hashSet.size(); //작으면 HashSet
     }
 }
