ec11837035963b9a78319eb739b5d13a56ff1d21
Sanghun Kim <rlatkdgns042@naver.com>
2024-04-03 21:01:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.57 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> hashMap = new HashMap<>();
+        
+        for (String player : participant) hashMap.put(player, hashMap.getOrDefault(player, 0) + 1);
+        
+        for (String player : completion) hashMap.put(player, hashMap.get(player) - 1);
+
+        for (String key : hashMap.keySet()) {
+            if (hashMap.get(key) != 0){
+                answer = key;
+            }
+        }
+        return answer;
+    }
+}
