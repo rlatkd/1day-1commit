3185e04628e7fd54b98d4b64668464831fd4f75b
Sanghun Kim <rlatkdgns042@naver.com>
2024-04-04 08:36:35 +0900

    modified:   "algorithm/\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/1/42576.\342\200\205\354\231\204\354\243\274\355\225\230\354\247\200\342\200\205\353\252\273\355\225\234\342\200\205\354\204\240\354\210\230/\354\231\204\354\243\274\355\225\230\354\247\200\342\200\205\353\252\273\355\225\234\342\200\205\354\204\240\354\210\230.java" 	deleted:    "sql/\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/2/131120.\342\200\2053\354\233\224\354\227\220\342\200\205\355\203\234\354\226\264\353\202\234\342\200\205\354\227\254\354\204\261\342\200\205\355\232\214\354\233\220\342\200\205\353\252\251\353\241\235\342\200\205\354\266\234\353\240\245\355\225\230\352\270\260/3\354\233\224\354\227\220\342\200\205\355\203\234\354\226\264\353\202\234\342\200\205\354\227\254\354\204\261\342\200\205\355\232\214\354\233\220\342\200\205\353\252\251\353\241\235\342\200\205\354\266\234\353\240\245\355\225\230\352\270\260.sql" 	deleted:    "sql/\355\224\204\353\241\234\352\267\270\353\236\230\353\250\270\354\212\244/2/131120.\342\200\2053\354\233\224\354\227\220\342\200\205\355\203\234\354\226\264\353\202\234\342\200\205\354\227\254\354\204\261\342\200\205\355\232\214\354\233\220\342\200\205\353\252\251\353\241\235\342\200\205\354\266\234\353\240\245\355\225\230\352\270\260/README.md"

--- algorithm/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ algorithm/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,18 +3,32 @@ import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
+
+        //마라톤 참여한 선수들 participant
+        //완주한 선수들 completion
+
+        //완주하지 못한 선수들
+        String nonCompletion = "";
+
+        //map을 이용하여 <선수이름, 횟수>
+        //참여자이면 횟수 + 1
+        //완주했으면 횟수 - 1
+        //그럼 완주 못 했으면 횟수가 0이 아니겠네
         Map<String, Integer> hashMap = new HashMap<>();
         
-        for (String player : participant) hashMap.put(player, hashMap.getOrDefault(player, 0) + 1);
+        //getOrDefault: 찾는 키 존재하면 키 반환 | 없다면 default값 반환
+        for (String player : participant)
+            hashMap.put(player, hashMap.getOrDefault(player, 0) + 1);
         
-        for (String player : completion) hashMap.put(player, hashMap.get(player) - 1);
+        //완주했으면 해당하는 선수의 value를 - 1 해서 0으로 만들면됨
+        for (String player : completion)
+            hashMap.put(player, hashMap.get(player) - 1);
 
-        for (String key : hashMap.keySet()) {
-            if (hashMap.get(key) != 0){
-                answer = key;
-            }
+
+        for (String player : hashMap.keySet()) {
+            if (hashMap.get(player) != 0)  //선수의 value가 0이 아니면 완주 못 한 사람
+                nonCompletion = player;
         }
-        return answer;
+        return nonCompletion;
     }
 }
