14f5e8d42c79c5d457a491e918d02aa403f393a2
김상훈 <rlatkdgns042@naver.com>
2024-06-26 21:57:54 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.02 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -0,0 +1,109 @@
+class Solution {
+    
+    public int solution(String[] board) {
+        
+        int o = 0;
+        int x = 0;
+        int oWin = 0;
+        int xWin = 0;
+        
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
+        		if (board[i].charAt(j) == 'O') {
+        			o++;
+        		}
+        		else if (board[i].charAt(j) == 'X') {
+        			x++;
+        		}
+        	}        	
+        }
+        
+        //승리자 체크 전 걸러내기
+        if (o-x>1 || o-x<0) {
+        	return 0;
+        }        
+        
+        //승리자 체크
+        if(o>=3) {
+        	oWin = winnerCheck(board, 'O');     	
+        }
+        
+        if (x>=3) {
+        	xWin = winnerCheck(board, 'X');
+        }
+        
+        //둘다 승리했을 경우
+        if (oWin==1 && xWin==1) {
+        	return 0;
+        }
+        
+        //O 승리 시
+        if (oWin == 1) {
+        	if (o-x == 1) {
+        		return 1;
+        	}
+        	else {
+        		return 0;
+        	}
+        }
+        
+        //X 승리 시
+        if (xWin == 1) {
+        	if (x == o) {
+        		return 1;
+        	}
+        	else {
+        		return 0;
+        	}
+        }
+
+		//나머지 상황
+		return 1;
+		
+    }
+    
+    public int winnerCheck(String[] board, char ox) {
+    	
+    	boolean bl = true;
+        
+        //가로체크
+        for (int i=0; i<3; i++) {
+        	bl = true;
+        	for (int j=0; j<3; j++) {
+        		if (board[i].charAt(j) != ox) {
+        			bl = false;     		
+        		}
+        	}
+            
+        	if (bl) {
+        		return 1;
+        	}
+        }
+        
+        //세로체크
+        for (int i=0; i<3; i++) {
+        	bl = true;
+        	for (int j=0; j<3; j++) {
+        		if (board[j].charAt(i) != ox) {
+        			bl = false;     		
+        		}
+        	}
+            
+        	if (bl) {
+        		return 1;
+        	}
+        }
+
+        //대각선체크
+        if (board[0].charAt(0) == ox && board[1].charAt(1) == ox && board[2].charAt(2) == ox) {
+        	return 1;
+        }
+        
+        if (board[0].charAt(2) == ox && board[1].charAt(1) == ox && board[2].charAt(0) == ox) {
+        	return 1;
+        }
+        
+        return 0;
+
+    }
+}
