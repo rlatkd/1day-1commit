b21797e280da945705434c41652b8bb4b2e321ef
김상훈 <rlatkdgns042@naver.com>
2024-07-17 00:58:12 +0900

    [level 2] Title: 점 찍기, Time: 15.32 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    public long solution(int k, int d) {
+        
+        long answer = 0;
+        
+        for (long x=0; x<=d; x+=k) {
+            long maxY = (long) Math.sqrt((long)d * d - (long)x * x);
+            answer += maxY/k+1;
+        }
+
+        return answer;
+    
+    }
+    
+}
