c1a9ed7071996eb1aa9cc7735b0604cb2cc9ab06
김상훈 <rlatkdgns042@naver.com>
2024-07-18 23:50:03 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 41.46 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,29 +1,22 @@
 import java.util.*;
-
 class Solution {
-    
-    public List<Integer> getDiv(int a) {
-        
+    public List<Integer> get_div(int a) {
         List<Integer> arr = new ArrayList<>();
-        
-        // 숫자의 제곱근까지 반복
-        for(int i=1; i<=Math.sqrt(a); i++) {
+        for(int i = 1; i <= Math.sqrt(a); i++) {
             if(a % i == 0) {
                 arr.add(i);
                 arr.add(a / i);
             }
         }
-        return arr; // 약수 리스트
+        return arr;
     }
-    
-    // 최대공약수
     public int problem(int[] arr1, int[] arr2) {
-        List<Integer> div = getDiv(arr1[0]);
-        Collections.sort(div, Collections.reverseOrder()); // 내림차순
+        List<Integer> div = get_div(arr1[0]);
+        Collections.sort(div, Collections.reverseOrder());
         int i, j;
-        boolean GCD; // 최대공약수?
-        for (i=0; i<div.size()-1; i++) {
-            GCD = false; // 초기화
+        boolean GCD;
+        for(i = 0; i < div.size() - 1; i++) {
+            GCD = false;
             int num = div.get(i); //현재 숫자
             for(j = 0; j < arr1.length; j++) {
                 if(arr1[j] % num != 0)
@@ -31,12 +24,12 @@ class Solution {
                     break;
                 }
             }
-            if (j==arr1.length)
+            if(j == arr1.length)
             {
                 GCD = true;                
             }
-            if (GCD) { // 최대공약수일 때
-                for (j=0; j<arr2.length; j++) {
+            if(GCD) {
+                for(j = 0; j < arr2.length; j++) {
                     if(arr2[j] % num == 0)
                     {
                         break;
@@ -48,19 +41,15 @@ class Solution {
         }
         return 0;
     }
-    
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
         int 철수;
         int 영희;
         Arrays.sort(arrayA);
         Arrays.sort(arrayB);
-        
-        // 각자의 최대공약수
         철수 = problem(arrayA, arrayB);
         영희 = problem(arrayB, arrayA);
         answer = 철수 > 영희 ? 철수 : 영희;
         return answer;
     }
-    
 }
